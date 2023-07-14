09cb0b2376102720c6a43329be1a73c14da87e3c
Sviatoslav <svatoslavvolk@gmail.com>
2023-07-14 16:00:22 +0200

    Final сommit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 document.querySelectorAll('.header__logo-img, .header__logo-link').forEach(function(element) {
     element.addEventListener('mouseover', function() {
-        document.querySelector('.header__logo').src = "dist/img/header__logo-hover.svg";
-        document.querySelector('.header__logo-link').style.color = "#8D81AC";
+        document.querySelector('.header__logo').src = "dist/img/header-logo.svg";
+        document.querySelector('.header__logo-link').style.color = "#fff";
     });
 
     element.addEventListener('mouseout', function() {
-        document.querySelector('.header__logo').src = "dist/img/header-logo.svg";
-        document.querySelector('.header__logo-link').style.color = "#fff";
+        document.querySelector('.header__logo').src = "dist/img/header__logo-hover.svg";
+        document.querySelector('.header__logo-link').style.color = "#8D81AC";
     });
 });
 
